ec7b02d931d1f837d1cd4ff5e8407babb6ba3541
Taewan-Gu <fksk94@gmail.com>
2022-11-16 00:13:29 +0900

    387

---
+++ python/22.11/387.py
@@ -0,0 +1,10 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        dic = collections.defaultdict(int)
+        for i in s:
+            dic[i] += 1
+        
+        for i in range(len(s)):
+            if dic[s[i]] == 1:
+                return i
+        return -1
