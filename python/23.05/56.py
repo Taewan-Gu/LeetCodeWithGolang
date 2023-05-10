7b340dfd7db28f3491a16ca0941c5ac36c0c1be5
tagrn <fksk94@gmail.com>
2023-05-10 22:34:58 +0900

    56

---
+++ python/23.05/56.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        ans = []
+        ts, te = intervals[0][0], intervals[0][1]
+        for s, e in intervals:
+            if ts <= s <= te:
+                te = max(e, te)
+            else:
+                ans.append([ts, te])
+                ts = s
+                te = max(e, te)
+        if not ans or ans[-1][0] != ts or ans[-1][1] != te:
+            ans.append([ts, te])
+
+        return ans
