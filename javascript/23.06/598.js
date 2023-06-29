15ffca244af12a92529f97868224288034c93fae
tagrn <fksk94@gmail.com>
2023-06-29 09:06:12 -0700

    598

---
+++ javascript/23.06/598.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} ops
+ * @return {number}
+ */
+var maxCount = function(m, n, ops) {
+    for (op of ops) {
+        m = Math.min(op[0], m)
+        n = Math.min(op[1], n)
+    }
+
+    return m * n
+};
