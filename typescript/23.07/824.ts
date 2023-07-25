f329928669ec1bd7a65270cd6c2b673fe2429bcf
tagrn <fksk94@gmail.com>
2023-07-25 13:16:06 -0700

    824

---
+++ typescript/23.07/824.ts
@@ -0,0 +1,15 @@
+function toGoatLatin(sentence: string): string {
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    let endWord = "maa"
+    let ans = ""
+    for (const word of sentence.split(" ")) {
+        if (vowels.includes(word[0])) {
+            ans += word + endWord + " "
+        } else {
+            ans += word.slice(1) + word[0] + endWord + " "
+        }
+        endWord += "a"
+    }
+
+    return ans.slice(0, ans.length-1)
+};
