60267ae6706604cb58a7d182289d813ab029fdb2
tagrn <fksk94@gmail.com>
2023-09-10 01:03:39 +0900

    373

--- python/23.09/373.py
+++ python/23.09/373.py
@@ -1,23 +1,18 @@
+from heapq import heappush, heappop
+
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        i, j = 0, 0
-        while k > (i+1) * (j+1):
-            if i+1 >= len(nums1):
-                j += 1
-            elif j+1 >= len(nums2):
-                i += 1
-            elif nums1[i+1] < nums2[j+1]:
-                i += 1
-            else:
-                j += 1
-        nums1 = nums1[:i+100]
-        nums2 = nums2[:j+100]
-
+        tmp = [(nums1[0] + nums2[0], 0, 0)]
         ans = []
-        for i in nums1:
-            for j in nums2:
-                ans.append([i,j])        
-        ans.sort(key=lambda x: sum(x))
+        visit = dict()
+        for _ in range(min(k, len(nums1) * len(nums2))):
+            s, i, j = heappop(tmp)
+            if j+1 < len(nums2) and not visit.get((i, j+1)):
+                heappush(tmp, (nums1[i] + nums2[j+1], i, j+1))
+                visit[(i, j+1)] = True
+            if i+1 < len(nums1) and not visit.get((i+1, j)):
+                heappush(tmp, (nums1[i+1] + nums2[j], i+1, j))
+                visit[(i+1, j)] = True
+            ans.append([nums1[i], nums2[j]])
 
-        return ans[:k]
-
+        return ans
